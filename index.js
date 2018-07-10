3f9fa247938ed72d1b8b5416a8c67203e3a71490
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-10 09:57:25 +0530

    English to Braille Assignment completed with linting fixed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
 /**
  * Converts text in english to braille code.
  * Write all code related to the DOM Manipulation here.
@@ -5,3 +7,16 @@
  * in english language to that in the equivalent braille
  * code.
  */
+function convert() {
+  const inputText = document.getElementById('sourceLangText').value;
+  const splitString = inputText.split('');
+  let i;
+  const englishToBrailleLiteral = new Map(englishToBrailleLiteralSet);
+  for (i = 0; i < splitString.length; i += 1) {
+    splitString[i] = englishToBrailleLiteral.get(splitString[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = splitString.join('');
+}
+window.onload = function () {
+  document.getElementById('btnConvertEnglishToBraille').addEventListener('click', convert);
+};
